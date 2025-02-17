c4b6f184a7c48bce9da8ba60601a290de811ea59
DaniloNogueira-Silva <danilo.nogueira1802@gmail.com>
2025-02-17 20:10:50 -0300

    build: Iniciar projeto

---
+++ src/routes.tsx
@@ -0,0 +1,47 @@
+// Icon Imports
+import {
+  MdBarChart,
+  MdHome,
+  MdLock,
+  MdOutlineShoppingCart,
+  MdPerson,
+} from 'react-icons/md';
+
+import React from 'react';
+
+const routes = [
+  {
+    name: 'Main Dashboard',
+    layout: '/admin',
+    path: 'default',
+    icon: <MdHome className="h-6 w-6" />,
+  },
+  {
+    name: 'NFT Marketplace',
+    layout: '/admin',
+    path: 'nft-marketplace',
+    icon: <MdOutlineShoppingCart className="h-6 w-6" />,
+
+    secondary: true,
+  },
+  {
+    name: 'Data Tables',
+    layout: '/admin',
+    icon: <MdBarChart className="h-6 w-6" />,
+    path: 'data-tables',
+  },
+  {
+    name: 'Profile',
+    layout: '/admin',
+    path: 'profile',
+    icon: <MdPerson className="h-6 w-6" />,
+  },
+  {
+    name: 'Sign In',
+    layout: '/auth',
+    path: 'sign-in',
+    icon: <MdLock className="h-6 w-6" />,
+  },
+
+];
+export default routes;
